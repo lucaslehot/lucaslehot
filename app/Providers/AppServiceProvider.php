b702570b364266386ed8b420e5edc006a889f3ef
Lucas Lehot <lehotlucas@gmail.com>
2022-04-05 17:59:59 +0200

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        $this->app->bind('path.public', function() {
+            return base_path().'/../public_html';
+        });
     }
 }
