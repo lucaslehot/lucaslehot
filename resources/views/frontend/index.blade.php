4069976178a467d9e3695a1a8439f05a9a0e6e47
Lucas Lehot <lehotlucas@gmail.com>
2022-05-02 15:43:29 +0200

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -11,7 +11,9 @@
         @stack('before-styles')
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href="{{ mix('css/frontend.css') }}" rel="stylesheet">
+        {{-- <link href="{{ mix('css/frontend.css') }}" rel="stylesheet"> --}}
+        <link rel="stylesheet" href="{{  asset('/css/frontend.css')  }}">
+
         <style>
             html, body {
                 background-color: #fff;
